0f23b568875699d451b72b2a4662ba6d92372e73
Yelin Kim <121251705+yesyell@users.noreply.github.com>
2023-10-03 22:29:09 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,36 +1,41 @@
 import streamlit as st
+from rembg import remove
+from PIL import Image
+from io import BytesIO
+import base64
 
-# 타이틀 적용 예시
-st.title('이것은 타이틀 입니다')
+st.set_page_config(layout="wide", page_title="Image Background Remover")
 
-# 특수 이모티콘 삽입 예시
-# emoji: https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
-st.title('스마일 :sunglasses:')
+st.header('What diseases can be predicted from chest X-ray images?')
+# st.write("Try uploading an image to watch the background magically removed.")
 
-# Header 적용
-st.header('헤더를 입력할 수 있어요! :sparkles:')
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
-# Subheader 적용
-st.subheader('이것은 subheader 입니다')
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
 
-# 캡션 적용
-st.caption('캡션을 한 번 넣어 봤습니다')
+def fix_image(upload):
+    image = Image.open(upload)
+    col1.write("X-ray Image :camera:")
+    col1.image(image)
 
-# 코드 표시
-sample_code = '''
-def function():
-    print('hello, world')
-'''
-st.code(sample_code, language="python")
+    fixed = remove(image)
+    col2.write("Result :wrench:")
+    col2.success('pneumothorax', icon="✅")
+    # st.sidebar.markdown("\n")
+    # st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
 
-# 일반 텍스트
-st.text('일반적인 텍스트를 입력해 보았습니다.')
 
-# 마크다운 문법 지원
-st.markdown('streamlit은 **마크다운 문법을 지원**합니다.')
-# 컬러코드: blue, green, orange, red, violet
-st.markdown("텍스트의 색상을 :green[초록색]으로, 그리고 **:blue[파란색]** 볼트체로 설정할 수 있습니다.")
-st.markdown(":green[$\sqrt{x^2+y^2}=1$] 와 같이 latex 문법의 수식 표현도 가능합니다 :pencil:")
+col1, col2 = st.columns(2)
+my_upload = st.sidebar.file_uploader("Upload an image :gear:", type=["png", "jpg", "jpeg"])
 
-# LaTex 수식 지원
-st.latex(r'\sqrt{x^2+y^2}=1')
+if my_upload is not None:
+    if my_upload.size > MAX_FILE_SIZE:
+        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
+    else:
+        fix_image(upload=my_upload)
+else:
+    fix_image("./test.jpg")
