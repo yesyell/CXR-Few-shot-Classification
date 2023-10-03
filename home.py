e55900fbc4e910dc1982dde674670fe1f551b686
Yelin Kim <121251705+yesyell@users.noreply.github.com>
2023-10-03 13:55:06 +0900

    home

--- home.py
+++ home.py
@@ -1,23 +1,13 @@
 import streamlit as st
 from PIL import Image
+from st_pages import Page, Section, show_pages, add_page_title
 
 st.title('Medical Image Project - by M3 :apple:')
-# emoji: https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
-# st.title('스마일 :sunglasses:')
 
 st.header('Few-shot Classification of Chest X-ray Disease')
 st.subheader('MIMIC 데이터셋과 CLIP을 이용한 구조화된 임상적 소견 예측')
-st.image(Image.open('/Users/kim-yelin/Documents/DeepDaiv/demo/주제.png'))
-
-# st.bar_chart(
-#   data={'time': [0, 1, 2, 3, 4, 5, 6], 'stock_value': [100, 200, 150, 300, 450, 500, 600]},
-#   x='time',
-#   y='stock_value'
-# )
-
-# st.file_uploader('Browse files')
-
-st.image(Image.open('/Users/kim-yelin/Documents/DeepDaiv/demo/텍스트.png'))
+st.image(Image.open('주제.png'))
+st.image(Image.open('텍스트.png'))
 
 col1, col2 = st.columns(2)
 
@@ -45,18 +35,7 @@ with col2:
     )
 
 st.write('Template : ', pathology, 'in the', location)
-# st.caption('캡션을 한 번 넣어 봤습니다')
-# st.text('일반적인 텍스트를 입력해 보았습니다.')
 
-# 컬러코드: blue, green, orange, red, violet
-# st.markdown("텍스트의 색상을 :green[초록색]으로, 그리고 **:blue[파란색]** 볼트체로 설정할 수 있습니다.")
-
-# LaTex 수식 지원
-# st.latex(r'\mathcal{L}_{\mathrm{LSES}} = \mathrm{log} \left( 1+\sum^C_{i=1}e^{-y_i\gamma s_i} \right)')
-
-from st_pages import Page, Section, show_pages, add_page_title
-
-# add_page_title() 
 show_pages([
         Page("home.py", "Home", "🏠"),
         Page("test.py", "Test", icon="🫁"),
